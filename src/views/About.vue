cc39027ee8c2d9a0d57703b820fb04b1b93b0afd
artwallace <1663489+artwallace@users.noreply.github.com>
2021-07-31 13:56:45 -0500

    create scaffolding

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Unofficial companion app for Spectre Operations</h1>
+  </div>
+</template>
---
+++ src/views/Aids.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Player Aids</h1>
+  </div>
+</template>
